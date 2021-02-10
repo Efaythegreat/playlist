7fbf90455f3a5eaec46561ccd94185429072df73
Glitch (yifei-playlist) <none>
2021-02-10 02:22:57 +0000

    🚗🌪 Updated with Glitch

---
+++ js/script.js
@@ -0,0 +1,33 @@
+
+
+let songNames = [
+  "Arashi no Saxophone 5",
+  "Jingle Jangle Jingle",
+  "Trainwreck of Electro Swing"
+];
+
+let artists = [
+  "King of Fighters",
+  "Kay Kiser and His Orchestra",
+  "Pascal Michael Stiefel"
+];
+
+let songLengths = ["5:43", "3:17", "4:07"];
+
+let links = [
+  "https://www.youtube.com/watch?v=vwqpjLfmjeQ",
+  "https://www.youtube.com/watch?v=1DD_NCM_RJs",
+  "https://www.youtube.com/watch?v=2c1iSpk3u1A"
+];
+
+let images = [
+  "https://i.ytimg.com/vi/mIJtBZL-RaE/sddefault.jpg",
+  "https://i.ytimg.com/vi/1DD_NCM_RJs/hqdefault.jpg",
+  "https://i1.sndcdn.com/artworks-000124149113-q5mjuo-t500x500.jpg"
+];
+
+$(".hide").click(function(){
+  $(".inputz").toggle();
+  $(".hide").text("show");
+  
+});
