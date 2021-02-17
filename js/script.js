d9109d6cf1bf75595bfbc5f522bf80b61915eb04
Glitch (yifei-playlist) <none>
2021-02-17 02:10:31 +0000

    🐥🏁 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -26,8 +26,48 @@ let images = [
   "https://i1.sndcdn.com/artworks-000124149113-q5mjuo-t500x500.jpg"
 ];
 
+
 $(".hide").click(function(){
   $(".inputz").toggle();
-  $(".hide").text("show");
+  
+  if($(".hide").text() == "hide"){
+    $(".hide").text("show");
+  }else{
+    $(".hide").text("hide");
+  }
+});
+
+for (let a of images){
+ 
+  $(".image").append("<p><img  src="+a+"></p>");
+  console.log(a)
+};
+
+for (let a of songNames){
+ 
+  $(".songName").append("<p class='item'>"+a+"</p>");
+  
+  console.log(a)
+};
+
+for (let a of artists){
+  $(".artist").append("<p class='item'>"+a+"</p>");
+  
+}
+
+for (let a of songLengths){
+  $(".songLength").append("<p class='item'>"+a+"</p>");
+  
+}
+
+for (let a of links){
+  $(".link").append("<a href='"+a+"'><p class='item'>link</p></a>");
+}
+
+
+
+$(".create").click(function(){
+  
+  
   
 });
