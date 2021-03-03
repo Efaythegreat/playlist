7f5f4d8c9f61b533ed9ffe89be516876e87cb9ad
Glitch (yifei-playlist) <none>
2021-03-03 01:59:41 +0000

    📅🛬 Updated with Glitch

--- js/script.js
+++ js/script.js
@@ -26,6 +26,48 @@ let images = [
   "https://i1.sndcdn.com/artworks-000124149113-q5mjuo-t500x500.jpg"
 ];
 
+function itemCreate(){
+  for (let a of images){
+    $(".image").append("<p><img src="+a+"></p>");
+    console.log(a)
+  };
+
+  for (let a of songNames){ 
+    $(".songName").append("<p class='item'>"+a+"</p>");
+  };
+
+  for (let a of artists){
+    $(".artist").append("<p class='item '>"+a+"</p>"); 
+  }
+
+  for (let a of songLengths){
+    $(".songLength").append("<p class='item'>"+a+"</p>");
+  }
+
+  for (let a of links){
+    $(".link").append("<a target='_blank' href='"+a+"'><p class='item'>Link</p></a>");
+  }
+}
+
+
+function apend(){
+  images.push($(".imageInp").val() );
+  songNames.push($(".nameInp").val());
+  artists.push($(".artistInp").val());
+  songLengths.push($(".lengthInp").val());
+  links.push($(".linkInp").val());
+}
+
+function empty(){
+  $(".image").empty();
+  $(".songName").empty();
+  $(".artist").empty();
+  $(".songLength").empty();
+  $(".link").empty();
+  
+}
+
+itemCreate();
 
 $(".hide").click(function(){
   $(".inputz").toggle();
@@ -37,37 +79,21 @@ $(".hide").click(function(){
   }
 });
 
-for (let a of images){
- 
-  $(".image").append("<p><img  src="+a+"></p>");
-  console.log(a)
-};
-
-for (let a of songNames){
- 
-  $(".songName").append("<p class='item'>"+a+"</p>");
-  
-  console.log(a)
-};
-
-for (let a of artists){
-  $(".artist").append("<p class='item'>"+a+"</p>");
-  
-}
-
-for (let a of songLengths){
-  $(".songLength").append("<p class='item'>"+a+"</p>");
-  
-}
-
-for (let a of links){
-  $(".link").append("<a href='"+a+"'><p class='item'>link</p></a>");
-}
-
 
 
 $(".create").click(function(){
-  
-  
+  if( $(".imageInp").val() == "" || 
+      $(".nameInp").val() == "" ||
+      $(".artistInp").val() == "" ||
+      $(".lengthInp").val() == "" ||
+      $(".linkInp").val() == ""
+){
+    $(".alert").text("You need to fill out everything before you add a song");
+  }else{
+    apend();
+    empty();
+    itemCreate()
+  }
   
 });
+
